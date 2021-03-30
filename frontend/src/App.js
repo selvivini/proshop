4f21d48f946b65fba0aa146decfaa32be13cf9c8
vinitha <selvivini@gmail.com>
2021-03-30 11:57:25 -0500

    addedd header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
    <>
-   Wlcome to Proshop!
+   <Header/>
+   <main>
+  <h1>Welcome to Proshop!</h1> 
+  </main>
+  <Footer/>
    </>
   );
 }
