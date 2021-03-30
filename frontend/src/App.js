6693c878000f3741bbea1cd25a057e6de37dfbc1
vinitha <selvivini@gmail.com>
2021-03-30 15:03:11 -0500

    added nav, footer and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import {Container} from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -7,7 +8,9 @@ function App() {
    <>
    <Header/>
    <main>
-  <h1>Welcome to Proshop!</h1> 
+     <Container className= "py-3">
+     <h1>Welcome to Proshop!</h1> 
+     </Container>
   </main>
   <Footer/>
    </>
