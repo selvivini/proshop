d5cc94a1330afce6df99a9595d82d3bb8fe383dc
vinitha <selvivini@gmail.com>
2021-04-01 13:04:13 -0500

    modified routes baed on fields set in db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import connectDb from './config/db.js'
-import products from './data/products.js'
+
 import dotEnv from 'dotenv'
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
 
 dotEnv.config()
 const app = express()
@@ -15,18 +16,13 @@ console.log(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000
 
+
 app.get('/', (req,res)=>{
     res.send("Api is running...")
 })
 
-app.get('/api/products', (req,res)=>{
-res.json(products)
-})
+app.use('/api/products',productRoutes)
 
-app.get('/api/products/:id', (req,res)=>{
-  const product = products.find(product=> product._id === req.params.id)
-    res.json(product)
-    })
 
 app.listen(PORT, ()=>{
  console.log( `Server listening in ${process.env.NODE_ENV} mode on Port ${PORT}`.magenta.bold)  
