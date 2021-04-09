6020744108e25662906e226d32302e7cbc9b4175
vinitha <selvivini@gmail.com>
2021-04-08 22:09:46 -0500

    added user registration in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
-
+import generateToken from '../utils/generateToken.js'
 
 
 
@@ -14,16 +14,60 @@ const authUser =  asyncHandler(async(req,res)=>{
 
    if(user && (await user.matchPassword(password))){
      res.json({
-         _id: (await user)._id,
+         _id: user._id,
          name: user.name,
          email: user.email,
          isAdmin: user.isAdmin,
-         token: null
+         token: generateToken(user._id)
      })
    }else{res.status(401)
    throw new Error('ivalid email or password')
 }
 })
 
+//register a new user
+//@route post'/api/users
+//@acess public route
+const registerUser =  asyncHandler(async(req,res)=>{
+    const {name,email,password} = req.body
+ 
+    const userExists = await User.findOne({email})
+    if(userExists){
+        res.status(400)
+        throw new Error('user already exists')
+    }
+    const user =await User.create({name, email, password})
+    if(user){
+        res.status(201).json({id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)})
+    }else{
+        res.status(400)
+        throw new Error('invalid user data')
+    }
+ })
+ 
 
-export {authUser}
+//get user profile
+//@route get('/api/users/profile)
+//@acess private route
+const getUserProfile =  asyncHandler(async(req,res)=>{
+    const user = await User.findById(req.user._id)
+    if(user){
+  res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+  })
+    }else{
+        res.status(401)
+        throw new Error('user not found')
+    }
+ })
+ 
+
+
+export {authUser, getUserProfile, registerUser}
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,6 +29,16 @@ const userSchema = mongoose.Schema({
 userSchema.methods.matchPassword = async function(enteredPassword){
 return await bcrypt.compare(enteredPassword, this.password)
 }
+
+//to save the hashed password in database
+//pre is the middleware in mongoose which runs before the password is saved
+userSchema.pre('save', async function(next){
+if(!this.isModified('password')){
+    next()
+}
+const salt = await bcrypt.genSalt(10);
+ this.password = await bcrypt.hash(this.password, salt)
+})
 const User = mongoose.model('User', userSchema)
 
 export default User
