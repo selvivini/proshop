464d46b3613a5618209afca09c863e73a213a3fe
vinitha <selvivini@gmail.com>
2021-04-09 18:55:19 -0500

    added actions, reducers for userlogin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,17 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 function App() {
   return (
    <Router>
    <Header/>
    <main>
      <Container className= "py-3">
-     <Route path= '/' component = {HomeScreen} exact/>
+     <Route path= '/login' component = {LoginScreen}/>
      <Route path= '/product/:id' component = {ProductScreen} exact/>
      <Route path= '/cart/:id?' component = {CartScreen}/>
+     <Route path= '/' component = {HomeScreen} exact/>
      </Container>
   </main>
   <Footer/>
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,7 @@
 import{USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS,USER_LOGIN_FAIL,USER_LOGIN_LOGOUT} from '../constants/userConstants'
 import axios from 'axios'
+
+
 export const login =(email,password)=>async(dispatch) =>{
 try {
   dispatch({type: USER_LOGIN_REQUEST})
