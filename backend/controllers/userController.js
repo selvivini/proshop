fd82e001b9711edc1bc007847d859478e5bc2215
vinitha <selvivini@gmail.com>
2021-04-14 11:47:02 -0500

    added updateuser profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,6 +68,31 @@ const getUserProfile =  asyncHandler(async(req,res)=>{
     }
  })
  
+ //update user profile
+//@route put('/api/users/profile)
+//@acess private route
+const updateUserProfile =  asyncHandler(async(req,res)=>{
+    const user = await User.findById(req.user._id)
+    
+    if(user){
+       user.name = req.body.name || user.name
+       user.email = req.body.email || user.email
+       if(req.body.password){
+     user.password = req.body.password
+       }
+      const updatedUser = await user.save()
+      res.json({
+          _id: updatedUser._id,
+          name: updatedUser.name,
+          email: updatedUser.email,
+          isadmin: updatedUser.isAdmin,
+          token: generateToken(updatedUser._id)
+      })
+    }else{
+        res.status(404)
+        throw new Error('user not found')
+    }
+ })
 
 
-export {authUser, getUserProfile, registerUser}
+export {authUser, getUserProfile, registerUser, updateUserProfile}
